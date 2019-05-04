ca46f347c3778e4474c91e6a557581f5b907202d
Joshua Hunsche Jones <joshua@hunschejones.com>
2019-05-03 19:40:54 -0700

    Update dependancies
    * Update sequelize from 4.43.0 to 5.8.4 (tested in staging)
    * Disable `operatorsAliases: false,` no longer supported in sequelize v5
    * update newrelic
    * update nodemon
    * Upgrade dotenv and change require statement to use new `.config()` syntax
    * Upgrade body-parser, compression, express-rate-limit, helmet, mocha, and supertest with no code changes needed

--- test/album-controller-spec.js
+++ test/album-controller-spec.js
@@ -1,5 +1,5 @@
 const should = require('should')
-require('dotenv').load();
+require('dotenv').config();
 const album_controller = require('../controllers/album.controller')
 process.env.NODE_ENV = 'development';
 
